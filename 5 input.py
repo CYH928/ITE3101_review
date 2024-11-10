a2a5e30692ee62708901d03328018cdd9b163315
yukhon <240095666@stu.vtc.edu.hk>
2024-11-10 23:09:24 +0800

    Delete 5 input.py

--- 5 input.py
+++
@@ -1,6 +0,0 @@
-message = input("What is your input: ") # input 
-print("Your message is -> "+message)
-
-# but can't
-def cant():
-    print(message + 1) # PLEASE print the same type, can't (string + integer)
