1622872c1231dc15c31c9f87e09b235a9dc00c99
yukhon <240095666@stu.vtc.edu.hk>
2024-11-10 23:09:33 +0800

    Delete 1 print_text.py

--- 1 print_text.py
+++
@@ -1,7 +0,0 @@
-# Text must use the include quote
-
-print('Hello, world!') # quotation marks ''
-print("Hello, world!") # double quotation marks ""
-# Both output: 'Hello, world!'
-
-print("Can use" + " PLUS " + "to connect the same type strings") # Connect words
