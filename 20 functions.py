5597bf8fdfc7c4e72f3de1976241a79426483338
yukhon <240095666@stu.vtc.edu.hk>
2024-11-12 00:05:10 +0800

    Update 20 functions.py

--- 20 functions.py
+++ 20 functions.py
@@ -1,11 +1,32 @@
+string = "Hello"
+integer = 100
+floats = 1.0000000000000000
 lists = [12, 4, 8, 72, 106, 483, 7, 52]
 
-print(len(lists))
-
-print(min(lists))
-
-print(max(lists))
-
-print(sum(lists))
-
 print()
+
+# String functions
+len(string)
+
+string.lower()
+
+string.upper()
+
+string.isalpha()
+
+string.isdigit()
+
+
+# Number functions
+max(lists)
+
+min(lists)
+
+abs(lists)
+
+
+# Type
+type(string)
+type(integer)
+type(floats)
+type(lists)
